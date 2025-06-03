c42aea722d636df6ac10e3604392f96de4397843
Gabe Mendoza <thatguyin@beanie.gg>
2025-06-02 21:47:25 -0500

    Fix build error: remove unused Image import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import Image from "next/image";
 import { translations, Language } from "./translations";
 
 export default function Home() {
